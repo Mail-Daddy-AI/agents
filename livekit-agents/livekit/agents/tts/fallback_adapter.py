9d630cd79869c2f3267488927b558a019b885f6d
Ujjawal <ujjawalrr@gmail.com>
2026-05-12 08:51:06 +0530

    added logging in tts fallback_adapter

--- livekit-agents/livekit/agents/tts/fallback_adapter.py
+++ livekit-agents/livekit/agents/tts/fallback_adapter.py
@@ -253,7 +253,7 @@ class FallbackChunkedStream(ChunkedStream):
             tts = adapter._tts_instances[i]
             tts_status = self._tts._status[i]
             if tts_status.available or all_failed:
-                logger.debug(f"tts.FallbackAdapter request_id={request_id} sending request to {tts.label}")
+                logger.info(f"tts.FallbackAdapter request_id={request_id} sending request to {tts.label}")
                 try:
                     resampler = (
                         rtc.AudioResampler(
@@ -277,7 +277,7 @@ class FallbackChunkedStream(ChunkedStream):
                         for rf in resampler.flush():
                             output_emitter.push(rf.data.tobytes())
 
-                    logger.debug(f"tts.FallbackAdapter request_id={request_id} received response from {tts.label} successfully after {time.time() - start_time} seconds")
+                    logger.info(f"tts.FallbackAdapter request_id={request_id} received response from {tts.label} successfully after {time.time() - start_time} seconds")
                     return
                 except Exception:  # exceptions already logged inside _try_synthesize
                     logger.warning(f"tts.FallbackAdapter request_id={request_id} request to {tts.label} failed")
@@ -413,7 +413,7 @@ class FallbackSynthesizeStream(SynthesizeStream):
                 tts = adapter._tts_instances[i]
                 tts_status = self._fallback_adapter._status[i]
                 if tts_status.available or all_failed:
-                    logger.debug(f"tts.FallbackAdapter request_id={request_id} sending streaming request to {tts.label}")
+                    logger.info(f"tts.FallbackAdapter request_id={request_id} sending streaming request to {tts.label}")
                     try:
                         new_input_ch = aio.Chan[str | SynthesizeStream._FlushSentinel]()
 
@@ -457,7 +457,7 @@ class FallbackSynthesizeStream(SynthesizeStream):
                             else:
                                 output_emitter.push(synthesized_audio.frame.data.tobytes())
 
-                        logger.debug(f"tts.FallbackAdapter request_id={request_id} received streaming response from {tts.label} successfully after {time.time() - start_time} seconds")
+                        logger.info(f"tts.FallbackAdapter request_id={request_id} received streaming response from {tts.label} successfully after {time.time() - start_time} seconds")
                         return
                     except Exception:
                         logger.warning(f"tts.FallbackAdapter request_id={request_id} streaming request to {tts.label} failed")
